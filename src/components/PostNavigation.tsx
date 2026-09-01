500171a4946cfeeef51b871d24872884ca4256ee
decollozq <decollzoq@gmail.com>
2026-09-01 16:15:42 +0900

    refactor: 이전글 다음글 위치 변경 및 git ignore 파일 수정

--- src/components/PostNavigation.tsx
+++ src/components/PostNavigation.tsx
@@ -1,47 +1,46 @@
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
-import { PostNav } from "../types/post";
+import {IoIosArrowBack} from "react-icons/io";
+import {IoIosArrowForward} from "react-icons/io";
+import {PostNav} from "../types/post";
 
 interface NavigationProps {
     prevPost?: PostNav | null;
     nextPost?: PostNav | null;
 }
 
-function PostNavigation({ prevPost, nextPost }: NavigationProps) {
+function PostNavigation({prevPost, nextPost}: NavigationProps) {
     return (
         <nav className="flex justify-between items-center space-x-8">
-            <div className="w-1/2">
-                {prevPost && (
-                    <a
-                        href={`/posts/${prevPost.slug}`}
-                        className="p-4 flex space-x-4 items-center w-full text-gray-900 dark:text-gray-50 dark:hover:bg-gray-700 hover:bg-gray-50 rounded-lg hover:text-primary"
-                    >
-                        <IoIosArrowBack className="text-2xl" />
-                        <div className="flex flex-col items-start gap-1 min-w-0">
-                            <div className="text-gray-500 dark:text-gray-300 text-sm">
-                                이전 글
-                            </div>
-                            <div className="w-full text-xl leading-10 text-start truncate ">
-                                {prevPost.title}
-                            </div>
-                        </div>
-                    </a>
-                )}
-            </div>
-
             <div className="w-1/2">
                 {nextPost && (
                     <a
                         href={`/posts/${nextPost.slug}`}
-                        className="p-4 flex space-x-4 items-center w-full justify-end text-gray-900 dark:text-gray-50 dark:hover:bg-gray-700 hover:bg-gray-50 rounded-lg hover:text-primary"
+                        className="p-4 flex space-x-4 items-center w-full text-gray-900 dark:text-gray-50 dark:hover:bg-gray-700 hover:bg-gray-50 rounded-lg hover:text-primary"
                     >
-                        <div className="flex flex-col items-end gap-1 min-w-0">
+                        <IoIosArrowBack className="text-2xl" />
+                        <div className="flex flex-col items-start gap-1 min-w-0">
                             <h2 className="text-gray-500 dark:text-gray-300  text-sm">
                                 다음 글
                             </h2>
-                            <p className="w-full text-xl leading-10 text-end truncate">
+                            <p className="w-full text-xl leading-10 text-start truncate">
                                 {nextPost.title}
                             </p>
+                        </div>
+                    </a>
+                )}
+            </div>
+            <div className="w-1/2">
+                {prevPost && (
+                    <a
+                        href={`/posts/${prevPost.slug}`}
+                        className="p-4 flex space-x-4 items-center w-full  justify-end text-gray-900 dark:text-gray-50 dark:hover:bg-gray-700 hover:bg-gray-50 rounded-lg hover:text-primary"
+                    >
+                        <div className="flex flex-col items-end gap-1 min-w-0">
+                            <div className="text-gray-500 dark:text-gray-300 text-sm">
+                                이전 글
+                            </div>
+                            <div className="w-full text-xl leading-10 text-end truncate ">
+                                {prevPost.title}
+                            </div>
                         </div>
                         <IoIosArrowForward className="text-2xl" />
                     </a>
