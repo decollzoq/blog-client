f98aaf0aedbede73a66e05bd55b76e443300472b
decollozq <decollzoq@gmail.com>
2026-09-03 20:57:03 +0900

    feat: Home 및 PostDetails 로딩 상태에 스켈레톤 UI 적용

--- src/routes/layouts/Default.tsx
+++ src/routes/layouts/Default.tsx
@@ -1,17 +1,9 @@
-import { Outlet } from "react-router";
-import { ScrollRestoration } from "react-router";
+import {Outlet} from "react-router";
+import {ScrollRestoration} from "react-router";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { CategoryProvider } from "../../contexts/providers/CategoryProvider";
-import { Suspense } from "react";
-import DefferedComponent from "../../components/DefferedComponent";
-import Home from "../pages/Home";
-import PostDetails from "../pages/PostDetails";
+import {CategoryProvider} from "../../contexts/providers/CategoryProvider";
 
-import {
-    HomeLoadingSkeleton,
-    PostDetailSkeleton,
-} from "../../components/LoadingSkeleton";
 export default function DefaultLayout() {
     return (
         <>
@@ -24,23 +16,3 @@ export default function DefaultLayout() {
         </>
     );
 }
-
-<Suspense
-    fallback={
-        <DefferedComponent>
-            <HomeLoadingSkeleton />
-        </DefferedComponent>
-    }
->
-    <Home />
-</Suspense>;
-
-<Suspense
-    fallback={
-        <DefferedComponent>
-            <PostDetailSkeleton />
-        </DefferedComponent>
-    }
->
-    <PostDetails />
-</Suspense>;
